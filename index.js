3bd37c4566e496bd21d9fbe57c604b1e15fb8500
Philippe Coval <p.coval@samsung.com>
2019-03-25 16:09:21 +0100

    lib: Add index.js for IoT.js
    Change-Id: I8fcb6c971f5094befdead824ab9891da67aebf9f
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ index.js
@@ -0,0 +1,6 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: BSD
+// Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+
+module.exports = require('./lib');
+
